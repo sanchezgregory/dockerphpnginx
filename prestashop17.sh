952a4d2f8024e8ac45a588bc91d2cd8990e71edb
Gregory Sanchez <gsanchezaper@gmail.com>
2020-08-12 20:43:45 -0300

    ajustando Nginx default

--- prestashop17.sh
+++ prestashop17.sh
@@ -6,17 +6,14 @@ wget https://www.prestashop.com/download/old/prestashop_1.7.5.2.zip
 # Unzip the PrestaShop archive
 unzip prestashop_1.7.5.2.zip
 
-# Create prestashop directory
-mkdir prestashop
-
 # Move zip file with actual shop to prestashop prestashop directory
-mv prestashop.zip prestashop
+mv prestashop.zip www/
 
 # Move index.php to prestashop directory
-mv index.php prestashop
+mv index.php www/
 
 #Set the correct user and group ownership for the PrestaShop directory
-sudo chown -R $user:www-data prestashop/
+sudo chown -R $user:www-data www/
 
 # Remove zip and install file
 rm prestashop_1.7.5.2.zip Install_PrestaShop.html
